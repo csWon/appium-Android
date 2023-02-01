4bf43899b2d407870d6b56ab7603ada49b10d8c2
csWon <dobee0101@gmail.com>
2023-02-02 00:32:22 +0900

    ua 변경

--- DriverManager.py
+++ DriverManager.py
@@ -24,14 +24,24 @@ class driverManager:
 
     def get_ua(self):
         ua_list = [
-                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_0_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
-                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0',
-                    'Mozilla/5.0 (X11; Linux i686; rv:107.0) Gecko/20100101 Firefox/107.0',
-                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0',
-                    'Mozilla/5.0 (X11; Linux i686; rv:102.0) Gecko/20100101 Firefox/102.0',
-                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/107.0.1418.68',
-                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/107.0.1418.68',
+            'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5359.79 Mobile Safari/537.36',
+            'Mozilla/5.0 (iPad; CPU OS 16_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/108.0.5359.52 Mobile/15E148 Safari/604.1',
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 16_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/108.0.5359.52 Mobile/15E148 Safari/604.1',
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 16_1_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Mobile/15E148 Safari/604.1',
+            'Mozilla/5.0 (iPad; CPU OS 16_1_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Mobile/15E148 Safari/604.1',
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 13_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/107.0 Mobile/15E148 Safari/605.1.15',
+            'Mozilla/5.0 (iPad; CPU OS 13_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/107.0 Mobile/15E148 Safari/605.1.15',
+            'Mozilla/5.0 (Android 13; Mobile; rv:68.0) Gecko/68.0 Firefox/107.0',
+            'Mozilla/5.0 (Android 13; Mobile; LG-M255; rv:107.0) Gecko/107.0 Firefox/107.0',
+
+            # 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+            # 'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_0_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+            # 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0',
+            # 'Mozilla/5.0 (X11; Linux i686; rv:107.0) Gecko/20100101 Firefox/107.0',
+            # 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0',
+            # 'Mozilla/5.0 (X11; Linux i686; rv:102.0) Gecko/20100101 Firefox/102.0',
+            # 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/107.0.1418.68',
+            # 'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/107.0.1418.68',
                     ]
         ua = random.choice(ua_list)
         print(ua)
@@ -59,21 +69,21 @@ class driverManager:
         # dc["browserName"] = "chrome"
         # dc["browserVersion"] = "109.0.5414.86"
 
-        # chrome_options = webdriver.ChromeOptions()
+        chrome_options = webdriver.ChromeOptions()
         # # 옵션 설정
         # chrome_options.add_argument("platformName:Android")
         # chrome_options.add_argument("platformVersion:9.0")
         # chrome_options.add_argument("deviceName:ce10171ab8ac5f3e04")
         # chrome_options.add_argument("browserName:chrome")
         # chrome_options.add_argument("browserVersion:109.0.5414.117")
-        # user_agent = self.get_ua()
-        # chrome_options.add_argument('user-agent=' + user_agent)
+        user_agent = self.get_ua()
+        chrome_options.add_argument('user-agent=' + user_agent)
 
         # 브라우저 열기
         # self.driver = webdriver.Chrome(executable_path='C:\chromedriver\chromedriver.exe', options=chrome_options)
         # self.driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", dc)
 
-        self.driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", dc)
+        self.driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", dc, options=chrome_options)
         # driver.get('https://www.aladin.co.kr/')
         start = time.time()
 
@@ -95,9 +105,9 @@ class driverManager:
         while True:
             try:
                 self.driver.implicitly_wait(60)
-                self.driver.get('https://www.naver.com/')
+                self.driver.get('http://20.214.201.213/index.html')
             except:
-                self.driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", dc)
+                self.driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", dc, options=chrome_options)
                 print("ERROR: Waiting for connection with appium server !!!")
                 time.sleep(1)
             else:
