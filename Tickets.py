011f6a2f574590d71578579ce6e60549401c09a3
csWon <dobee0101@gmail.com>
2023-02-11 15:56:00 +0900

    멀티 프로세스 적용 전 merge

--- Tickets.py
+++ Tickets.py
@@ -1,6 +1,6 @@
 # import Kyobo
 from Kyobo import kyobo
-# from Yes24 import yes24
+from Yes24 import yes24
 from Aladin import aladin
 from DriverManager import driverManager
 
@@ -8,11 +8,21 @@ class tickets:
     def getTicket(self):
         testList_12124 = [
             # [aladin, '', 0, 0, '사랑의 이해'],
-            [kyobo, '', 0, 0, '구의 증명'],
+            # [kyobo, '', 0, 0, '구의 증명'],
             # [yes24, '', 0, 0, '이토록 평범한 미래'],
             # [kyobo, '', 0, 0, '클루지'],
             # [kyobo, '', 0, 0, '나도 모르는 내 마음의 심리법칙'],
             # [aladin, '', 0, 0, '넌 대체 몇 년째 영어 공부를 하고 있는 거니?'],
             # [aladin, '', 0, 0, '세상의 마지막 기차역'],
+            # [kyobo, '', 0, 0, '클루지'],
+            # [yes24, '', 0, 0, '물고기는 존재하지 않는다'],
+            # [yes24, '', 0, 0, '돈의 속성 200쇄 리커버'],
+            # [aladin, '', 0, 0, '쿼런틴'],
+            # [aladin, '', 0, 0, '쿼런틴'],
+            # [yes24, '', 0, 0, '돈의 속성 200쇄 리커버'],
+            # [aladin, '', 0, 0, '쿼런틴'],
+            [yes24, '', 0, 0, '물고기는 존재하지 않는다'],
+            [aladin, '', 0, 0, '쿼런틴'],
+            [yes24, '', 0, 0, '물고기는 존재하지 않는다'],
         ]
         return testList_12124
